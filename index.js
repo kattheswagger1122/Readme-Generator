d82f9e3d4b289968905452188706e1f1f9a08a8c
Katrina  Shih <katrinashih@Katrinas-MacBook-Pro.local>
2022-06-19 00:43:39 -0400

    added other two functions 1. write to file function and initializing app function

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-// TODO: Include packages needed for this application
-require("detenv").config();
+// TODO: Include packages needed for this applicationno
 const generateMarkdown = require('./utils/generateMarkdown.js');
 const inquirer = require("inquirer");
 const fs = require("fs");
@@ -59,10 +58,25 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+// fs application
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        // If there is an error, will report an error.
+        // If there is no error, will utput a message to indicate the markdown file has been successfully created.
+        err ? console.error(err) : console.log(`Your ${data.title} README.md file has been created.`)
+    );
+}
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    // Asks the user questions to complete the markedown file
+    inquirer.prompt(questions).then((answers) => {
+        const data = generateMarkdown(answers);
+        // Displays answers to questions
+        console.log(answers);
+        // Creates the markdown file based on the answers provided
+        writeToFile('README.md', data);
+    })
+}
 
 // Function call to initialize app
 init();
