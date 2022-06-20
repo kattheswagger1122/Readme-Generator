9a303e6ddc7ee95c36650daf32cab90193bb09ec
Katrina  Shih <katrinashih@Katrinas-MacBook-Pro.local>
2022-06-19 21:31:36 -0400

    added functions for license and other minor debugs

---
+++ README.md
@@ -0,0 +1,43 @@
+# readme stuff
+  ![License](https://img.shields.io/badge/License-APACHE 2.0-blue)
+  
+
+  ##Table of Contents
+  * [Description](#description)
+  * [Usage](#usefaq)
+  * [Installation](#install)
+  * [Testing](#test)
+  * [License](#license)
+  * [Contributing to This Repo](#contributefaq)
+  * [Questions / Contact Details](#questions)
+  
+  <a name='description'></a>
+  ## Description
+  good stuff
+  
+  <a name='usefaq'></a>
+  ## Usage
+  very pro
+  
+  <a name='install'></a>
+  ## Installation
+  npm i
+
+  <a name='test'></a>
+  ## Testing
+  npm test
+
+  <a name='contributefaq'></a>
+  ## Contributing to This Repo
+  it's a me KAT
+  
+  
+<a name='license'></a>
+## License
+it's a me KAT
+
+
+<a name='questions'></a>
+## Questions? Please contact me here:
+For additional questions, please contact [kat](https://github.com/kat) at kat's@email
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,11 +1,15 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  
+}
 
 // TODO: Create a function to generate markdown for README
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ![License](https://img.shields.io/badge/License-${data.license}-blue)
+  
 
   ##Table of Contents
   * [Description](#description)
@@ -19,24 +23,33 @@ function generateMarkdown(data) {
   <a name='description'></a>
   ## Description
   ${data.description}
+  
   <a name='usefaq'></a>
   ## Usage
   ${data.usefaq}
+  
   <a name='install'></a>
   ## Installation
   ${data.install}
+
   <a name='test'></a>
   ## Testing
   ${data.test}
-  <a name='license'></a>
-  ## License
-  The application is covered under a ${data.license} license.
+
   <a name='contributefaq'></a>
   ## Contributing to This Repo
   ${data.contributefaq}
-  <a name='questions'></a>
-  ## Questions / Contact Details
-  If you have any questions or need to contact me about this app, I can reached either on Github or by email at [${data.email}](${data.email}).
+  
+  
+<a name='license'></a>
+## License
+${data.contributefaq}
+
+
+<a name='questions'></a>
+## Questions? Please contact me here:
+For additional questions, please contact [${data.github}](https://github.com/${data.github}) at ${data.email}
+
 `;
 }
 
